f642511965bd12533b27913074199c463d811eb6
Denys Dovhan <email@denysdovhan.com>
2015-09-22 16:33:24 +0300

    Change jsx ext.

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 import './main.css';
 
 import React from 'react';
-import App from './App.jsx';
+import App from './App';
 
 const app = document.createElement('div');
 document.body.appendChild(app);
--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   module:{
     loaders: [
       {
-        test: /\.jsx?$/,
+        test: /\.js?$/,
         loaders: ['react-hot', 'babel'],
         include: path.resolve(__dirname, 'app')
       },
