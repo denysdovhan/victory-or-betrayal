ea28e0ea4a3da2bc56540cbac699a9acd8d80440
Denys Dovhan <email@denysdovhan.com>
2015-09-23 23:36:45 +0300

    Add input and heading.

--- app/App.js
+++ app/App.js
@@ -1,9 +1,45 @@
 import React from 'react';
 
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.finishEdit = this.finishEdit.bind(this);
+    this.checkEnter = this.checkEnter.bind(this);
+
+    this.state = {
+      value: ''
+    }
+  }
+
+  finishEdit(e) {
+    console.log(`Entered text: ${e.target.value}`);
+
+    this.setState({
+      value: e.target.value
+    });
+  }
+
+  checkEnter(e) {
+    if (e.key === 'Enter') {
+      this.finishEdit(e);
+    }
+  }
+
   render() {
+    const value = this.state.value !== '' ? this.state.value : "щось";
+
     return (
-      <h1>Hello, World!</h1>
+      <div>
+        <h1>Чи є "{value}" зрадою?</h1>
+
+        <input
+          type="text"
+          autoFocus={true}
+          onBlur={this.finishEdit}
+          onKeyPress={this.checkEnter}/>
+      </div>
     );
   }
 }
